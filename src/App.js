33663caa831ef1c9f5920bb21fd59650296955c3
VALKREE <49433750+VALKREE@users.noreply.github.com>
2021-07-28 16:47:45 +0400

    Загрузка файлов 2-ого ДЗ

--- src/App.js
+++ src/App.js
@@ -1,20 +1,98 @@
-import {Message} from "./components/Message/Message"
-
+import React, { useState } from 'react';
 import './App.sass';
 
-const myMessage = 'This is a message';
+const userId = 1;
+const username = 'User 1';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <Message name="Какое-то сообщение"/>
-        </p>
-      </header>
-      
-    </div>
-  );
+export default class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            messages: [
+                {
+                    id: 0,
+                    author: "AI: Alex",
+                    text: "Hello! I,m Alex!",
+                },
+            ]
+        }
+        this.user = username
+        this.sendMessage = this.sendMessage.bind(this)
+    }
+    sendMessage(text) {
+        let msg = text;
+        console.log(msg);
+        msg,
+    }
+    render() {
+        return (
+            <div className="app">
+              <MessageList 
+                  messages={this.state.messages} />
+              <SendMessageForm
+                  sendMessage={this.sendMessage}
+              />
+            </div>
+        );
+    }
 }
 
-export default App;
+class MessageList extends React.Component {
+    render() {
+        return (
+            <ul className="message-list">
+                {this.props.messages.map((message, index) => {
+                    return (
+                      <li  key={message.id} className="message">
+                        <div>{message.author}</div>
+                        <div>{message.text}</div>
+                      </li>
+                    )
+                })}
+            </ul>
+        )
+    }
+}
+
+class SendMessageForm extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            message: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    
+    handleChange(e) {
+        this.setState({
+            message: e.target.value
+        })
+
+    }
+    handleSubmit(e) {
+        e.preventDefault()
+        this.props.sendMessage(this.state.message)
+        this.setState({
+            message: ''
+        })
+    }
+    
+    render() {
+        return (
+            <form
+                onSubmit={this.handleSubmit}
+                className="message-form">
+                <input
+                    className="message-input"
+                    onChange={this.handleChange}
+                    value={this.state.message}
+                    placeholder="Введите ваше сообщение и нажмите Enter"
+                    type="text" />
+            </form>
+        )
+    }
+}
+
+
+
