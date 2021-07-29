fd6cc86db3ca622d7199d9ca2af20c101126b811
VALKREE <49433750+VALKREE@users.noreply.github.com>
2021-07-29 11:12:24 +0400

    Ready hw2

--- src/App.js
+++ src/App.js
@@ -1,98 +1,46 @@
-import React, { useState } from 'react';
+import React from 'react';
+import {useRef, useEffect, useState, useCallback} from 'react'
+
 import './App.sass';
+import {MessageList} from './components/MessageList'
+import {Form} from './components/Form'
+import {Message} from './components/Message'
+function App(){
+    const [messages,setMessages]=useState([{text: 'Welcome to chat!', author: "Chat", id: 0,}]);
 
-const userId = 1;
-const username = 'User 1';
+    const hanlerClick = () => {
+        setMessages(' ');
+    };
 
-export default class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            messages: [
-                {
-                    id: 0,
-                    author: "AI: Alex",
-                    text: "Hello! I,m Alex!",
-                },
-            ]
-        }
-        this.user = username
-        this.sendMessage = this.sendMessage.bind(this)
-    }
-    sendMessage(text) {
-        let msg = text;
-        console.log(msg);
-        msg,
-    }
-    render() {
-        return (
-            <div className="app">
-              <MessageList 
-                  messages={this.state.messages} />
-              <SendMessageForm
-                  sendMessage={this.sendMessage}
-              />
+    const handlerSending = useCallback((newMessage)=>{
+        setMessages([...messages, newMessage]);
+    }, [messages]);
+
+
+    useEffect(() => {
+        const timeout = setTimeout(()=>{
+            if (!messages.length || messages[messages.length - 1].author === "AI: Alex"){
+                return;
+            }
+            const newMessage = {
+                author: "AI: Alex",
+                text: "Hello! I'm Alex!",
+                id: 0,
+            }
+            setMessages([...messages, newMessage]);
+        }, 1000);
+        return () => clearTimeout(timeout);
+    }, [messages])
+
+    return (
+        <div className="app">
+            <div className="app_chat">
+                <MessageList messages={messages} />
+                <Form onSendMessage={handlerSending} />
             </div>
-        );
-    }
-}
-
-class MessageList extends React.Component {
-    render() {
-        return (
-            <ul className="message-list">
-                {this.props.messages.map((message, index) => {
-                    return (
-                      <li  key={message.id} className="message">
-                        <div>{message.author}</div>
-                        <div>{message.text}</div>
-                      </li>
-                    )
-                })}
-            </ul>
-        )
-    }
-}
-
-class SendMessageForm extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            message: ''
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
-    
-    handleChange(e) {
-        this.setState({
-            message: e.target.value
-        })
-
-    }
-    handleSubmit(e) {
-        e.preventDefault()
-        this.props.sendMessage(this.state.message)
-        this.setState({
-            message: ''
-        })
-    }
-    
-    render() {
-        return (
-            <form
-                onSubmit={this.handleSubmit}
-                className="message-form">
-                <input
-                    className="message-input"
-                    onChange={this.handleChange}
-                    value={this.state.message}
-                    placeholder="Введите ваше сообщение и нажмите Enter"
-                    type="text" />
-            </form>
-        )
-    }
+        </div>
+    );
 }
 
 
-
+export default App;
