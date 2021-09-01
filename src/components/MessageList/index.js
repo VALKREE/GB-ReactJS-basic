36af83a5ed871affde8769f784c18f5a6bfdaa3a
VALKREE <49433750+VALKREE@users.noreply.github.com>
2021-09-01 11:29:35 +0400

    Fix

--- src/components/MessageList/index.js
+++ src/components/MessageList/index.js
@@ -1,14 +1,23 @@
 import React, { useCallback } from "react";
 import { Message } from "../Message";
 
+
+import { makeStyles } from "@material-ui/core";
+const useStyles = makeStyles({
+
+});
+
 export const MessageList = ({ messages }) => {
+    const classes = useStyles();
   const renderMessage = useCallback(
     (mess) => (
-      <Message
-        text={mess.text}
-        author={mess.author}
-        key={mess.id}
-      />
+      <div className={classes.message}>
+          <Message
+              text={mess.text}
+              author={mess.author}
+              key={mess.id}
+          />
+      </div>
     ),
     []
   );
