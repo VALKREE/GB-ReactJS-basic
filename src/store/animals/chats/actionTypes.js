121d8cbdd98d2ef85d257d7a2a8d9c1442bbd6bd
VALKREE <49433750+VALKREE@users.noreply.github.com>
2021-09-10 13:51:28 +0400

    1

---
+++ src/store/animals/chats/actionTypes.js
@@ -0,0 +1,4 @@
+export const CLICK_CHAT = "CHATS::CLICK_CHAT";
+export const ADD_CHAT = "CHATS::ADD_CHAT";
+export const DELETE_CHAT = "CHATS::DELETE_CHAT";
+export const SEND_MESSAGE = "CHATS::SEND_MEESSAGE";
